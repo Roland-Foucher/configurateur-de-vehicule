7d7c0fc1d0cc0058de8bcceeff8ce98a3ba3ea2b
Roland Foucher <roland.foucher.pro@gmail.com>
2022-05-20 17:08:27 +0200

    add findOne et deleteOne dans le EditController Repository

--- configurateur-de-vehicule/src/main/java/co/simplon/alt3cda/configurateurdevehicule/controller/EditVehicleController.java
+++ configurateur-de-vehicule/src/main/java/co/simplon/alt3cda/configurateurdevehicule/controller/EditVehicleController.java
@@ -4,19 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import co.simplon.alt3cda.configurateurdevehicule.entity.Vehicle;
 import co.simplon.alt3cda.configurateurdevehicule.entity.VehicleDTO;
 import co.simplon.alt3cda.configurateurdevehicule.entityBuilder.VehicleFactory;
 import co.simplon.alt3cda.configurateurdevehicule.exception.VehicleNotInEnumException;
+import co.simplon.alt3cda.configurateurdevehicule.exception.VehiculeNotInDatabaseException;
+import co.simplon.alt3cda.configurateurdevehicule.repository.VehicleRepository;
 
 @RestController
 public class EditVehicleController {
   
   @Autowired
   VehicleFactory vehiculeFactory;
+
+  @Autowired
+  VehicleRepository vehicleRepository;
   
   @PostMapping("/save")
   public ResponseEntity<String> save(@RequestBody VehicleDTO vehicleDTO){
@@ -36,7 +43,25 @@ public class EditVehicleController {
  }
 
   @GetMapping("/{id}")
-  public ResponseEntity<VehicleDTO> getOne(@RequestParam int id){
-    return null;
+  public ResponseEntity<Vehicle> getOne(@PathVariable int id){
+    try {
+      Vehicle vehicle = vehiculeFactory.getVehicle(id);
+      return ResponseEntity.ok(vehicle);
+    } catch (VehicleNotInEnumException e) {
+      e.printStackTrace();
+      return ResponseEntity.internalServerError().build();
+    } catch (VehiculeNotInDatabaseException e) {
+      e.printStackTrace();
+      return ResponseEntity.internalServerError().build();
+    }
+  }
+
+  @GetMapping("/delete/{id}")
+  public ResponseEntity<String> deleteOne(@PathVariable int id){
+    vehicleRepository.deleteById(id);
+    if (vehicleRepository.findById(id).orElse(null) == null){
+      return ResponseEntity.ok().build();
+    }
+    return ResponseEntity.internalServerError().body("error when delete the vehicle");
   }
 }
